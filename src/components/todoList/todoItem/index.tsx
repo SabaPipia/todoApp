02858837ab39e4375adb1c7ba5fcb20a5729890f
Ch34ry <pipiasaba788@gmail.com>
2023-06-24 14:50:51 +0400

    improved TodoItem rendering

--- src/components/todoList/todoItem/index.tsx
+++ src/components/todoList/todoItem/index.tsx
@@ -1,14 +1,60 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Check, Remove } from "../../../icons";
 
 import "./style.scss";
-
-type TodoItemProps = {
-  id?: number;
-  value?: string;
+import { todoContext } from "../../wrapperComponent";
+type todo = {
+  text: string;
+  id: number;
+  selected: boolean;
 };
-const TodoItem: React.FC<TodoItemProps> = ({ id, value }) => {
-  console.log(id);
-  return <li key={id}>{value}</li>;
+
+interface TodoItemProps {
+  index: number;
+  item: todo;
+  selectTodo: (id: number) => void;
+  handleClick: (todo: todo) => void;
+  removeTodo: (todo: todo) => void;
+}
+const TodoItem: React.FC<TodoItemProps> = ({
+  index,
+  item,
+  selectTodo,
+  handleClick,
+  removeTodo,
+}) => {
+  const context = useContext(todoContext);
+  return (
+    <div className="todoItem" key={index}>
+      <div className="leftPart">
+        <div
+          className={`icon`}
+          style={{
+            opacity: item.selected ? "1" : "",
+          }}
+          onClick={() => selectTodo(item.id)}
+        >
+          <Check />
+        </div>
+        <span
+          style={{
+            textDecoration: item.selected ? "line-through" : "none",
+            color: item.selected ? "#8080808a" : "",
+            fontWeight: item.selected ? "500" : "300",
+          }}
+          onClick={() => {
+            context?.setEditButton(true);
+            handleClick(item);
+          }}
+        >
+          {item.text}
+        </span>
+      </div>
+      <div className="rightPart" onClick={() => removeTodo(item)}>
+        <Remove />
+      </div>
+    </div>
+  );
 };
 
 export default TodoItem;
